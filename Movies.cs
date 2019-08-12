ccb886e1b17bb02349ad898b65cf9d1ef2ae946b
Olaposi <ooomishope@yahoo.com>
2019-08-12 18:21:32 -0400

    lab 11

---
+++ Movies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    class Movies
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public Movies(string title, string genre)
+        {
+            Title = title;
+            Genre = genre;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movies> films = new List<Movies> {
+               new Movies("john wick", "action"),
+               new Movies("john wick 2", "action"),
+               new Movies("john wick 3", "action"),
+               new Movies("Scream", "horror"),
+               new Movies("The shining", "horror"),
+               new Movies("star wars", "scifi"),
+               new Movies("startrek", "scifi"),
+               new Movies("batman", "action"),
+               new Movies("Avengers", "action"),
+               new Movies("Scarface", "drama")
+            };
+            bool yes = true;
+            while (yes)
+            {
+                Menu(films);
+            }
+            
+        }
+        public static void Menu(List<Movies> filmList)
+        {
+
+            Console.WriteLine("What type of movie would you like to watch? \r\n action \r\n drama \r\n scifi \r\n horror \r\n quit");
+
+            string userInput = Console.ReadLine().ToLower();
+
+            foreach (Movies item in filmList)
+            {
+                if (item.Genre == userInput)
+                {
+                    Console.WriteLine(item.Title);
+                }
+                
+            }
+            if (userInput == "quit")
+            {
+                Console.WriteLine("Have a great day");
+                System.Environment.Exit(0);
+            }
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey(true);
+        }
+
+    }
+}
